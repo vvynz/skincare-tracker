e32f36489c9f0afd4b077f22c1f5468ae8a78717
Vivian Zhang <vvynz12@gmail.com>
2022-07-13 20:40:02 -0400

    Create a form to add new products

--- src/Components/productForm.js
+++ src/Components/productForm.js
@@ -1,5 +1,19 @@
 import React, { useState } from "react";
 
 export default function ProductForm() {
-  return ("I am a form");
+  return (
+    <div className="form-container">
+      <form className="product-form">
+        <input
+          className="form-field"
+          placeholder="Product name"
+          name="productName" />
+        <label>Date opened:</label>
+        <input type="date" />
+        <label>Expiry date:</label>
+        <input type="date" />
+        <button className="add-product-btn">Add</button>
+      </form>
+    </div>
+  );
 }

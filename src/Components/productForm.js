3ed746247c911fe9554f833617b3365a89c8a50b
Vivian Zhang <vvynz12@gmail.com>
2022-07-15 21:00:20 -0400

    Set up register and handleSubmit to useForm hook and create a onSubmit function that will alert user data

--- src/Components/productForm.js
+++ src/Components/productForm.js
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 import "../Styles/ProductForm.scss";
 
 export default function ProductForm() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => { alert(data) };
+
   return (
     <div className="form-container">
       <form className="product-form">

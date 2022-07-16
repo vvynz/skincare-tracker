a1a5c6921f6469cdf5aa596fc12bcff81d9c5f51
Vivian Zhang <vvynz12@gmail.com>
2022-07-15 21:12:32 -0400

    Set-up form fields with react-hook-form

--- src/Components/productForm.js
+++ src/Components/productForm.js
@@ -5,11 +5,11 @@ import "../Styles/ProductForm.scss";
 
 export default function ProductForm() {
   const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => { alert(data) };
+  const onSubmit = (data) => { console.log(data) };
 
   return (
     <div className="form-container">
-      <form className="product-form">
+      {/* <form className="product-form">
         <label>Product:</label>
         <input
           className="form-field"
@@ -20,6 +20,28 @@ export default function ProductForm() {
         <label>Expiry date:</label>
         <input type="date" />
         <button className="add-product-btn">Add</button>
+      </form> */}
+
+      <form className="product-form" onSubmit={handleSubmit(onSubmit)}>
+        <label>Product:</label>
+        <input
+          className="form-field"
+          type="text"
+          placeholder="Product name"
+          {...register("Product name", { required: true })} />
+
+        <label>Date opened:</label>
+        <input
+          type="datetime"
+          placeholder="yyyy-mm-dd"
+          {...register("Date opened", {})} />
+
+        <label>Expiry date:</label>
+        <input
+          type="datetime"
+          placeholder="yyyy-mm-dd"
+          {...register("Expiry date", {})} />
+        <button type="submit" className="add-product-btn">Add</button>
       </form>
     </div>
   );

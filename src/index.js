7895d6a6026d1ca767f2a648bd6765384f4781f9
Vivian Zhang <vvynz12@gmail.com>
2022-07-18 19:42:20 -0400

    import ChakraProvider and wrap it around the ColorModeScript and App component

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from './App';
 import theme from './theme';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider } from "@chakra-ui/react";
 import { ColorModeScript } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-    <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 

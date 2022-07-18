97e644673d6bc57b9bce8b97557fc12fdcd973d4
Vivian Zhang <vvynz12@gmail.com>
2022-07-18 19:02:48 -0400

    Create a NavBar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import NavBar from "./Components/NavBar";
 import ProductForm from "./Components/ProductForm";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <ProductForm />
     </div>
   );
---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export default function NavBar() {
+  return (
+    <header>
+      <h1>NAV</h1>
+    </header>
+  )
+}

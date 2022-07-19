5f544862ee38e58c833e287acf05c2855896a879
Vivian Zhang <vvynz12@gmail.com>
2022-07-18 21:29:10 -0400

    Minor stying update

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { useColorMode } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
+import { Button, Heading } from "@chakra-ui/react";
 
 export default function NavBar() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <header>
+      <Heading>🌼 skincare tracker.</Heading>
       <Button onClick={toggleColorMode}>
         Toggle {colorMode === "light" ? "dark" : "light"}
       </Button>

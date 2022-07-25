9e4828d67c770f6e87967a713ae17a06f82e1ec3
Vivian Zhang <vvynz12@gmail.com>
2022-07-25 19:41:04 -0400

    Import style components from chakra-ui and set up table to display current products in use

--- src/Components/InUse.jsx
+++ src/Components/InUse.jsx
@@ -1,10 +1,40 @@
 import React from "react";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer
+} from "@chakra-ui/react";
 
 export default function InUse(props) {
   const data = props.products;
-  // console.log(data)
+  console.log(data)
   return (
     <section>
+      <TableContainer>
+        <Table>
+          <TableCaption>Current Products In Use:</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Name</Th>
+              <Th>Date Opened:</Th>
+              <Th>Expiry Date:</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Th>{data.productName}</Th>
+              <Th>{data.dateOpened}</Th>
+              <Th>{data.expiryDate}</Th>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
     </section>
   )
 }

8e7d913fd18524c8fe1d9d1f3f9c4853be732a6e
Vivian Zhang <vvynz12@gmail.com>
2022-08-08 18:12:55 -0400

    Remove unused styling components

--- src/Components/InUse.jsx
+++ src/Components/InUse.jsx
@@ -1,37 +1,15 @@
 import React from "react";
 import {
-  Table,
-  Thead,
-  Tbody,
-  Tfoot,
   Tr,
   Th,
-  Td,
-  TableCaption,
-  TableContainer,
 } from "@chakra-ui/react";
 
 export default function InUse({ items }) {
- 
   return (
-      <TableContainer>
-        <Table>
-          <TableCaption>Current Products In Use:</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Name</Th>
-              <Th>Date Opened:</Th>
-              <Th>Expiry Date:</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            <Tr>
-              <Th>{items.itemName}</Th>
-              <Th>{items.dateOpened}</Th>
-              <Th>{items.expiryDate}</Th>
-            </Tr>
-          </Tbody>
-        </Table>
-      </TableContainer>
+    <Tr>
+      <Th>{items.itemName}</Th>
+      <Th>{items.dateOpened}</Th>
+      <Th>{items.expiryDate}</Th>
+    </Tr>
   );
 }

30d5d78289df70dec77cdefec9d3d99407204132
Vivian Zhang <vvynz12@gmail.com>
2022-08-09 19:28:04 -0400

    Create an InUseEditable component

---
+++ src/Components/InUseEditable.jsx
@@ -0,0 +1,16 @@
+import { Tr, Th, Input } from "@chakra-ui/react";
+
+export default function InUseEditable() {
+  return 
+  <Tr>
+    <Th>
+      <Input />
+    </Th>
+    <Th>
+      <Input />
+    </Th>
+    <Th>
+      <Input />
+    </Th>
+  </Tr>;
+}

f65004baa91f2c94e8046c3e9f8dbb2ae78d31c1
Vivian Zhang <vvynz12@gmail.com>
2022-08-09 18:32:35 -0400

    Minor update

--- src/Hooks/useAppData.js
+++ src/Hooks/useAppData.js
@@ -1,3 +1,9 @@
 import React from "react";
 import { nanoid } from "nanoid";
 import { useDisclosure } from "@chakra-ui/react";
+
+export default function useAppData() {
+  const appData = {};
+
+  return appData;
+}

63f452815dde3bd8bdb41ac10deaebc340ac6c92
Vivian Zhang <vvynz12@gmail.com>
2022-08-15 19:49:29 -0400

    Set up a form to add items in Wishlist component

--- src/Components/Wishlist.jsx
+++ src/Components/Wishlist.jsx
@@ -5,7 +5,30 @@ import { FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 export default function Wishlist() {
   return (
     <form>
-      
+      <FormControl>
+        <FormLabel>Brand:</FormLabel>
+        <Input
+          name="brand"
+          type="text"
+          required="required"
+          placeholder="Enter a brand name..."
+        />
+        <FormLabel>Item Name:</FormLabel>
+        <Input
+          name="itemName"
+          type="text"
+          required="required"
+          placeholder="Enter item name..."
+        />
+        <Button
+          type="submit"
+          colorScheme="purple"
+          size="md"
+          marginTop="10px"
+          borderRadius="10px"
+          variant="outline"
+        >Add</Button>
+      </FormControl>
     </form>
   );
 }

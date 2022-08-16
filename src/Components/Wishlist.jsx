10a1a4bd202a924454bb800d4d844e41556507bb
Vivian Zhang <vvynz12@gmail.com>
2022-08-15 20:10:49 -0400

    Create an add item button and import styling components from CHakra UI

--- src/Components/Wishlist.jsx
+++ src/Components/Wishlist.jsx
@@ -1,34 +1,55 @@
 import React from "react";
 
-import { FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+import "../Styles/Wishlist.scss";
 
 export default function Wishlist() {
   return (
-    <form>
-      <FormControl>
-        <FormLabel>Brand:</FormLabel>
-        <Input
-          name="brand"
-          type="text"
-          required="required"
-          placeholder="Enter a brand name..."
-        />
-        <FormLabel>Item Name:</FormLabel>
-        <Input
-          name="itemName"
-          type="text"
-          required="required"
-          placeholder="Enter item name..."
-        />
-        <Button
-          type="submit"
-          colorScheme="purple"
-          size="md"
-          marginTop="10px"
-          borderRadius="10px"
-          variant="outline"
-        >Add</Button>
-      </FormControl>
-    </form>
+    <section className="wishlist">
+      <nav>
+        <Button colorScheme="purple">Add Item</Button>
+        <form>
+          <FormControl>
+            <FormLabel>Brand:</FormLabel>
+            <Input
+              name="brand"
+              type="text"
+              required="required"
+              placeholder="Enter a brand name..."
+            />
+            <FormLabel>Item Name:</FormLabel>
+            <Input
+              name="itemName"
+              type="text"
+              required="required"
+              placeholder="Enter item name..."
+            />
+            <Button
+              type="submit"
+              colorScheme="purple"
+              size="md"
+              marginTop="10px"
+              borderRadius="10px"
+              variant="outline"
+            >
+              Add
+            </Button>
+          </FormControl>
+        </form>
+      </nav>
+    </section>
   );
 }

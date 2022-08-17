8f9baf43474f16873696d88b47360cc167643b0c
Vivian Zhang <vvynz12@gmail.com>
2022-08-17 19:02:30 -0400

    Resolve error where the table data wasn't being displayed

--- src/Components/Wishlist.jsx
+++ src/Components/Wishlist.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import { Tr, Th, Button } from "@chakra-ui/react";
 
-export default function Wishlist({ item }) {
+export default function Wishlist({ items }) {
   return (
     <Tr>
-      <Th>{item.brand}</Th>
-      <Th>{item.itemName}</Th>
+      <Th>{items.brand}</Th>
+      <Th>{items.itemName}</Th>
     </Tr>
   );
 }

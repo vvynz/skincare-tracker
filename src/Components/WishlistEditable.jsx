cfca2e47b20f76f10708857823533d6100132577
Vivian Zhang <vvynz12@gmail.com>
2022-08-17 21:17:40 -0400

    Set up input forms and a save and cancel button

--- src/Components/WishlistEditable.jsx
+++ src/Components/WishlistEditable.jsx
@@ -1,5 +1,13 @@
 import React from "react";
+import {Tr, Th, Button, Input} from "@chakra-ui/react"
 
 export default function Wishlist() {
-  return("ELLO");
+  return(<Tr>
+    <Th><Input name="brand" /></Th>
+    <Th><Input name="itemName" /></Th>
+    <Th>
+      <Button size="xs" colorScheme="purple" variant="outline">Save</Button>
+      <Button size="xs" colorScheme="purple" variant="outline">Cancel</Button>
+    </Th>
+  </Tr>);
 }

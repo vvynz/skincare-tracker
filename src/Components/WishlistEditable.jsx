239c2986a390a0f1638aa9c7f276450ff759dd4b
Vivian Zhang <vvynz12@gmail.com>
2022-08-22 19:56:23 -0400

    Pass the cancel function to the cancel button as an onClick event

--- src/Components/WishlistEditable.jsx
+++ src/Components/WishlistEditable.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import {Tr, Th, Button, Input} from "@chakra-ui/react"
 
-export default function Wishlist() {
+export default function Wishlist({cancel}) {
   return(<Tr>
     <Th><Input name="brand" /></Th>
     <Th><Input name="itemName" /></Th>
     <Th>
       <Button size="xs" colorScheme="purple" variant="outline">Save</Button>
-      <Button size="xs" colorScheme="purple" variant="outline">Cancel</Button>
+      <Button size="xs" colorScheme="purple" variant="outline" onClick={cancel}>Cancel</Button>
     </Th>
   </Tr>);
 }

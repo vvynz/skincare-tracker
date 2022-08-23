db329210c870659839b798165646c9ecadff954a
Vivian Zhang <vvynz12@gmail.com>
2022-08-23 19:25:49 -0400

    Rename logo files

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import { useColorMode } from "@chakra-ui/react";
 import { Button, Heading } from "@chakra-ui/react";
 
-import logo1 from "../Assets/logo1.png";
-import logo2 from "../Assets/logo2.png";
+import logo from "../Assets/logo2.png";
 
 import "../Styles/NavBar.scss";
 
@@ -12,7 +11,7 @@ export default function NavBar() {
   return (
     <header>
       <nav className="navbar">
-        <img className="logo" src={logo2} alt="logo" />
+        <img className="logo" src={logo} alt="Logo icon" />
         <Heading>skincare tracker.</Heading>
       </nav>
       <Button size="xs" onClick={toggleColorMode}>

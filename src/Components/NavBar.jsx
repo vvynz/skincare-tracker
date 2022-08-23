a4a03f7a03cf58d0ad016a5e1593c7b39bf0c380
Vivian Zhang <vvynz12@gmail.com>
2022-08-23 19:23:31 -0400

    Update logo styling

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,16 +2,22 @@ import React, { useState } from "react";
 import { useColorMode } from "@chakra-ui/react";
 import { Button, Heading } from "@chakra-ui/react";
 
+import logo1 from "../Assets/logo1.png";
+import logo2 from "../Assets/logo2.png";
+
 import "../Styles/NavBar.scss";
 
 export default function NavBar() {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <header>
-      <Heading>🌼 skincare tracker.</Heading>
+      <nav className="navbar">
+        <img className="logo" src={logo2} alt="logo" />
+        <Heading>skincare tracker.</Heading>
+      </nav>
       <Button size="xs" onClick={toggleColorMode}>
         Toggle {colorMode === "light" ? "dark" : "light"}
       </Button>
     </header>
-  )
+  );
 }

957c9e6795dff36a4b6194ed28d75b2c73c3cdf2
Vivian Zhang <vvynz12@gmail.com>
2022-08-26 20:22:32 -0400

    Minor styling updates

--- src/Components/WishlistEditable.jsx
+++ src/Components/WishlistEditable.jsx
@@ -3,8 +3,8 @@ import {Tr, Th, Button, Input} from "@chakra-ui/react"
 
 export default function Wishlist({cancel, editFormData, handleWishlistEditFormChange}) {
   return(<Tr>
-    <Th><Input name="brand" value={editFormData.brand}  onChange={handleWishlistEditFormChange} /></Th>
-    <Th><Input name="itemName" value={editFormData.itemName} onChange={handleWishlistEditFormChange} /></Th>
+    <Th><Input name="brand" value={editFormData.brand} required="required" onChange={handleWishlistEditFormChange} /></Th>
+    <Th><Input name="itemName" value={editFormData.itemName} required="required" onChange={handleWishlistEditFormChange} /></Th>
     <Th>
       <Button type="submit" size="xs" colorScheme="purple" variant="outline" >Save</Button>
       <Button size="xs" colorScheme="purple" variant="outline" onClick={cancel}>Cancel</Button>

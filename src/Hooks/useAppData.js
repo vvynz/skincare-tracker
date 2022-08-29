bc0c5435b9f7603747aa7a63a2f7dd1fc8e977aa
Vivian Zhang <vvynz12@gmail.com>
2022-08-29 19:05:13 -0400

    Create a generateDate function that will return the current date as a string

--- src/Hooks/useAppData.js
+++ src/Hooks/useAppData.js
@@ -5,5 +5,15 @@ import { useDisclosure } from "@chakra-ui/react";
 export default function useAppData() {
   const appData = {};
 
+  const generateDate = () => {
+    const date = new Date();
+    let d = date.getDate();
+    let m = date.getMonth() + 1;
+    let y = date.getFullYear();
+
+    return "" + y + "-" + (m <= 9? "0" + m : m) + "-" + (d <= 9 ? "0" + d : d);
+  }
+  appData.generateDate = generateDate;
+
   return appData;
 }

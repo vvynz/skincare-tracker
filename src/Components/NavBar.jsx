7f855c30d0af22e02d3345668d0c6fe4de15ce5e
Vivian Zhang <vvynz12@gmail.com>
2022-09-14 19:11:13 -0400

    Add a search bar to the navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useColorMode } from "@chakra-ui/react";
-import { Button, Heading } from "@chakra-ui/react";
+import { Button, Heading, Input } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 import logo from "../Assets/logo.png";
@@ -16,6 +16,7 @@ export default function NavBar() {
         <img className="logo" src={logo_col} alt="Logo icon" />
         <Heading>skincare tracker.</Heading>
       </nav>
+      <Input className="search_bar" width="auto" placeholder="Search..." />
       <button className="toggle--colorMode" onClick={toggleColorMode}>
         {colorMode === "light" ? <MoonIcon boxSize="1.5em" color="purple.700" /> : <SunIcon boxSize="1.5em"  />}
       </button>

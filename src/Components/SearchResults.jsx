05b2863a096eaa057eeacadd431bac7eb4e80a50
Vivian Zhang <vvynz12@gmail.com>
2022-10-19 19:24:35 -0400

    Import styling components from chakra ui

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -1,3 +1,23 @@
-export default function SearchResults({results}) {
-  return("🫶🏻")
+import {TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tbody,
+  Tr,
+  Th, Td} from "@chakra-ui/react";
+
+export default function SearchResults({ results }) {
+  console.log(results);
+  return (
+    <div>
+      {results.map((item) => {
+        return (
+          <section>
+            <h2>{item.brand}:</h2>
+            <h4>{item.itemName}</h4>
+          </section>
+        );
+      })}
+    </div>
+  );
 }

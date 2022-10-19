5d2ca3be24a87ec86b51b38d0f08a1119a72e959
Vivian Zhang <vvynz12@gmail.com>
2022-10-19 19:36:00 -0400

    Add conditional rendering so that if the results array is greater than 1, it will render the table of results

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -11,31 +11,35 @@ import {
 
 export default function SearchResults({ results }) {
   console.log(results);
-  return (
-    <TableContainer>
-      <Table>
-        <TableCaption>Results</TableCaption>
-        <Thead>
-          <Tr>
-            <Th>Brand:</Th>
-            <Th>Item Name:</Th>
-            <Th>Date Opened:</Th>
-            <Th>Expiry Date:</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {results.map((item) => {
-            return (
-              <Tr>
-                <Td>{item.brand}</Td>
-                <Td>{item.itemName}</Td>
-                <Td>{item.dateOpened}</Td>
-                <Td>{item.expiryDate}</Td>
-              </Tr>
-            );
-          })}
-        </Tbody>
-      </Table>
-    </TableContainer>
-  );
+  if (results.length > 0) {
+    return (
+      <TableContainer>
+        <Table>
+          <TableCaption>Results</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>Brand:</Th>
+              <Th>Item Name:</Th>
+              <Th>Date Opened:</Th>
+              <Th>Expiry Date:</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {results.map((item) => {
+              return (
+                <Tr>
+                  <Td>{item.brand}</Td>
+                  <Td>{item.itemName}</Td>
+                  <Td>{item.dateOpened}</Td>
+                  <Td>{item.expiryDate}</Td>
+                </Tr>
+              );
+            })}
+          </Tbody>
+        </Table>
+      </TableContainer>
+    );
+  } else {
+    return("Loading...")
+  }
 }

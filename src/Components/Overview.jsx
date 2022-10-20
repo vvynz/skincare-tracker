23e304b596b5569b0432980b2ed4be75f410d872
Vivian Zhang <vvynz12@gmail.com>
2022-10-20 19:01:49 -0400

    Minor styling update

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 
-import { Box } from "@chakra-ui/react";
+import { Box, Heading, Text } from "@chakra-ui/react";
 
 import "../Styles/Overview.scss";
 
 export default function Overview() {
   return (
     <main className="stats_container">
-      <Box w="10rem" h="10rem" className="stats_box">
-        Total In Rotation:
+      <Box w="15rem" h="10rem" className="stats_box">
+        <Text fontSize="xs" color="black">In Use:</Text>
+        <Text fontSize="2xl" as="b" color="black">5</Text>
+        {/* hardcoded stats, UPDATE LATER */}
       </Box>
-      <Box w="10rem" h="10rem" className="stats_box">
-        Wishlist:
+      <Box w="15rem" h="10rem" className="stats_box">
+        <Text fontSize="xs" color="black">Wishlist:</Text>
+        <Text fontSize="2xl" as="b" color="black">2</Text>
+        {/* ^^ hardcoded data, UPDATE LATER */}
       </Box>
     </main>
   );

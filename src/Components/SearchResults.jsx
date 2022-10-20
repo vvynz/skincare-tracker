dac90616476a37e8d07b79c401481dcb656785ec
Vivian Zhang <vvynz12@gmail.com>
2022-10-20 17:24:29 -0400

    Create a load function and move all the table results into the function

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -7,38 +7,48 @@ import {
   Tr,
   Th,
   Td,
+  Spinner,
 } from "@chakra-ui/react";
 
 export default function SearchResults({ query, filteredResults }) {
-  if (query) {
-    return (
-      <TableContainer>
-        <Table>
-          <TableCaption>Results</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Brand:</Th>
-              <Th>Item Name:</Th>
-              <Th>Date Opened:</Th>
-              <Th>Expiry Date:</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {filteredResults.map((item) => {
-              return (
+  
+  const load = () => {
+    <TableContainer>
+      <Table>
+        <TableCaption>Results</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Brand:</Th>
+            <Th>Item Name:</Th>
+            <Th>Date Opened:</Th>
+            <Th>Expiry Date:</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {filteredResults.map((item) => {
+            return (
+              <>
                 <Tr id={item.id}>
                   <Td>{item.brand}</Td>
                   <Td>{item.itemName}</Td>
                   <Td>{item.dateOpened}</Td>
                   <Td>{item.expiryDate}</Td>
                 </Tr>
-              );
-            })}
-          </Tbody>
-        </Table>
-      </TableContainer>
+              </>
+            );
+          })}
+        </Tbody>
+      </Table>
+    </TableContainer>;
+  };
+
+  if (query) {
+    return (
+      <>
+        <Spinner />
+      </>
     );
   } else {
-    return("Loading...")
+    return "HELLO";
   }
 }
